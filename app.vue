8ee22b730519d2ed2237912e370c52e5bbc62971
yarone cohen <yarone.cohen05@gmail.com>
2024-11-18 15:05:29 +0200

    home page

--- app.vue
+++ app.vue
@@ -4,10 +4,10 @@
     <Hero/>
 
     <Badge :number="'500+'" :description="'Vidéos gratuites'" :bgColor="'bg-orange-500'" :rightPosition="'right-[100px]'" :bottomPosition="'bottom-[150px]'" />
-    
-    <Badge :number="'1000+'" :description="'Clients satisfaits'" :bgColor="'bg-blue-500'" :rightPosition="'right-[100px]'" :bottomPosition="'bottom-[260px]'" />
+
+    <Badge :number="'1000+'" :description="'Clients satisfaits'" :bgColor="'bg-blue-500'" :rightPosition="'right-[200px]'" :bottomPosition="'bottom-[650px]'" />
    
-    <Badge :number="'50+'" :description="'Coachs disponibles'" :bgColor="'bg-lime-400'" :rightPosition="'right-[470px]'" :bottomPosition="'bottom-[460px]'" />
+    <Badge :number="'50+'" :description="'Coachs disponibles'" :bgColor="'bg-lime-400'" :rightPosition="'right-[490px]'" :bottomPosition="'bottom-[520px]'" />
     <Banner/>
 
 
